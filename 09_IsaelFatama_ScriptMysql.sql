842b4b327db22c9369d8f092ac69d76a6713ebb6
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-09-13 21:24:20 -0500

    Actividad MySql

---
+++ 09_IsaelFatama_ScriptMysql.sql
@@ -0,0 +1,94 @@
+DROP DATABASE IF EXISTS dbGamarraMarket;
+CREATE DATABASE dbGamarraMarket
+DEFAULT CHARACTER SET utf8;
+
+USE dbGamarraMarket;
+/*tabla cliente*/
+CREATE TABLE CLIENTE (
+    id int  NOT NULL,
+    tipo_documento char(3)  NOT NULL,
+    numero_documento char(15)  NOT NULL,
+    nombres varchar(60)  NOT NULL,
+    apellidos varchar(90)  NOT NULL,
+    email varchar(80)  NULL,
+    celular char(9)  NULL,
+    fecha_nacimiento date  NOT NULL,
+    activo bool  NOT NULL,
+    CONSTRAINT CLIENTE_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE PRENDA (
+    id int  NOT NULL,
+    descripcion varchar(90)  NOT NULL,
+    marca varchar(60)  NOT NULL,
+    cantidad int  NOT NULL,
+    talla varchar(10)  NOT NULL,
+    precio decimal(8,2)  NOT NULL,
+    activo bool  NOT NULL,
+    CONSTRAINT PRENDA_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE VENDEDOR (
+    id int  NOT NULL,
+    tipo_documento char(3)  NOT NULL,
+    numero_documento char(15)  NOT NULL,
+    nombres varchar(60)  NOT NULL,
+    apellidos varchar(90)  NOT NULL,
+    salario decimal(8,2)  NOT NULL,
+    celular char(9)  NULL,
+    email varchar(80)  NULL,
+    activo bool  NOT NULL,
+    CONSTRAINT VENDEDOR_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE VENTA (
+    id int  NOT NULL,
+    fecha_hora timestamp  NOT NULL,
+    activo bool  NOT NULL,
+    cliente_id int  NOT NULL,
+    vendedor_id int  NOT NULL,
+    CONSTRAINT VENTA_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE VENTA_DETALLE (
+    id int  NOT NULL,
+    cantidad int  NOT NULL,
+    venta_id int  NOT NULL,
+    prenda_id int  NOT NULL,
+    CONSTRAINT VENTA_DETALLE_pk PRIMARY KEY (id)
+);
+
+SHOW COLUMNS IN CLIENTE;
+SHOW COLUMNS IN PRENDA;
+SHOW COLUMNS IN VENDEDOR;
+SHOW COLUMNS IN VENTA;
+SHOW COLUMNS IN VENTA_DETALLE;
+SHOW TABLES;
+
+DROP TABLE CLIENTE;
+/*CLIENTE_VENTA*/
+ALTER TABLE VENTA ADD CONSTRAINT VENTA_CLIENTE FOREIGN KEY VENTA_CLIENTE (cliente_id)
+    REFERENCES CLIENTE (id);
+
+/*VENTA_DETALLE_PRENDA*/
+ALTER TABLE VENTA_DETALLE ADD CONSTRAINT VENTA_DETALLE_PRENDA FOREIGN KEY VENTA_DETALLE_PRENDA (prenda_id)
+    REFERENCES PRENDA (id);
+
+/*VENTA_DETALLE_VENTA*/
+ALTER TABLE VENTA_DETALLE ADD CONSTRAINT VENTA_DETALLE_VENTA FOREIGN KEY VENTA_DETALLE_VENTA (venta_id)
+    REFERENCES VENTA (id);
+
+/*VENTA_VENDEDOR*/
+ALTER TABLE VENTA ADD CONSTRAINT VENTA_VENDEDOR FOREIGN KEY VENTA_VENDEDOR (vendedor_id)
+    REFERENCES VENDEDOR (id);
+
+/* Listar relaciones de tablas de la base de datos activa */
+SELECT 
+    i.constraint_name, k.table_name, k.column_name, 
+    k.referenced_table_name, k.referenced_column_name
+FROM 
+    information_schema.TABLE_CONSTRAINTS i 
+LEFT JOIN information_schema.KEY_COLUMN_USAGE k 
+ON i.CONSTRAINT_NAME = k.CONSTRAINT_NAME 
+WHERE i.CONSTRAINT_TYPE = 'FOREIGN KEY' 
+AND i.TABLE_SCHEMA = DATABASE();
